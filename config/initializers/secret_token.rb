2928e2dfe713348ab99d7ed5fdacb5148d28fff0
Wilson Jung <wilsonfoz@gmail.com>
2012-05-25 10:27:12 -0300

    Commit inicial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GestaoEscolar::Application.config.secret_token = '90c719a8c46903bf7534225ddda222e6e0dc962645d2cb0b8576841f1341a59a5ccf31f5d3c51191bc9ee54310f38df1c5142e8798a37ec5821d4568b8b744ee'
