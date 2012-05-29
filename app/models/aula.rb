c6790c9c4aad9f6fffab094db3b157fbe14b8861
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 02:18:51 -0300

    Ajuste no model Aula.

--- app/models/aula.rb
+++ app/models/aula.rb
@@ -3,8 +3,6 @@ class Aula < ActiveRecord::Base
   belongs_to :professor
   belongs_to :turma
 
-  #accepts_nested_attributes_for :materia, :professor
-
   attr_accessible :materia_id, :professor_id
 
   validates_uniqueness_of :materia_id, :scope => [:turma_id]
