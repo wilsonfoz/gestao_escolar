1cd02d7583dfa177a1a618ec16fd15d2d4a478bd
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 11:02:46 -0300

    Exibição de mensagem em index para inexistência de turmas.

--- app/helpers/turmas_helper.rb
+++ app/helpers/turmas_helper.rb
@@ -1,2 +1,11 @@
+#encoding: UTF-8
 module TurmasHelper
+  def nenhuma_turma
+	"<tr>
+	  <td>Não há turmas cadastradas.</td>
+	  <td></td>
+	  <td></td>
+	  <td></td>
+	</tr>".html_safe
+  end			
 end
