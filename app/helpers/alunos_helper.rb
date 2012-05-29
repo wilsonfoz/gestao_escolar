7c420e6280683ef12b4eef0e78e381e61ae73afb
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 10:28:49 -0300

    Validação da presença de nome no cadastro de alunos e exibição de mensagem em index para inexistência de alunos.

--- app/helpers/alunos_helper.rb
+++ app/helpers/alunos_helper.rb
@@ -1,2 +1,13 @@
+#encoding: UTF-8
 module AlunosHelper
+
+  def nenhum_aluno
+	"<tr>
+	  <td>Não há alunos cadastrados</td>
+	  <td></td>
+	  <td></td>
+	  <td></td>
+	  <td></td>
+	</tr>".html_safe
+  end
 end
--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,4 +1,6 @@
 class Aluno < ActiveRecord::Base
   attr_accessible :nome, :turma_id
   belongs_to :turma
+
+  validates_presence_of :nome
 end
