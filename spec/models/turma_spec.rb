31cad2b7a18d38483280d79f92319c9f9584bcde
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 11:14:56 -0300

    Teste para nome em branco em turma_spec.

--- spec/models/turma_spec.rb
+++ spec/models/turma_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe Turma do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Turma do	
+  let(:turma) {FactoryGirl.build(:turma)}
+	
+  it "should be valid" do
+  	turma.should be_valid
+  end
+
+  it "should not be valid with blank 'nome'" do
+  	turma.nome = ""
+  	turma.should_not be_valid
+  end
 end
