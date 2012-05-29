887bacebc4629470aa07ea0a6bb94a9a55886e50
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 13:52:08 -0300

    Teste de aceitação para index de alunos.

--- spec/requests/alunos_spec.rb
+++ spec/requests/alunos_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe "Alunos" do
-  describe "GET /alunos" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get alunos_path
-      response.status.should be(200)
+  describe "vendo alunos" do
+    it "exibe todos os alunos" do
+      FactoryGirl.create(:aluno, :nome => "Davi")
+      visit alunos_path
+      page.should have_content("Davi")
     end
   end
 end
