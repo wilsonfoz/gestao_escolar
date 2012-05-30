ed641e6fb3f607c2780b56507a469354fe83e315
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 22:58:29 -0300

    Implementado teste unitário para turma.

--- spec/models/turma_spec.rb
+++ spec/models/turma_spec.rb
@@ -1,14 +1,24 @@
+#encoding: UTF-8
 require 'spec_helper'
 
-describe Turma do	
-  let(:turma) {FactoryGirl.build(:turma)}
-	
-  it "should be valid" do
-  	turma.should be_valid
+describe Turma do
+  it { should have_many(:alunos) }
+  it { should have_many(:aulas) }
+  it { should have_many(:professores).through(:aulas) }
+  it { should have_many(:materias).through(:aulas) }
+
+  it "deve possuir um nome" do
+	Turma.new(:nome => "").should_not be_valid
   end
 
-  it "should not be valid with blank 'nome'" do
-  	turma.nome = ""
-  	turma.should_not be_valid
+  it "deve possuir um nome único" do
+  	Turma.create(:nome => "Teste")
+  	should validate_uniqueness_of(:nome)
   end
+
+  it "to_s deve retornar o nome" do
+    turma = Turma.create(:nome => "5A")
+	turma.to_s.should == "5A"
+  end
+
 end
