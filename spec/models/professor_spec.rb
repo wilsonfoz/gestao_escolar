c68188074ee42c8d988e8c1e63c4fb4075da3071
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 21:43:55 -0300

    Implementado teste unitário para professor.

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,5 +1,18 @@
+#encoding: UTF-8
 require 'spec_helper'
 
 describe Professor do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:aulas) }
+  it { should have_many(:materias).through(:aulas) }
+  it { should have_many(:turmas).through(:aulas) }
+
+  it "deve possuir um nome" do
+	Professor.new(:nome => "").should_not be_valid
+  end
+
+  it "to_s deve retornar o nome" do
+	professor = Professor.create(:nome => "José")
+	professor.to_s.should == "José"
+  end
+
 end
