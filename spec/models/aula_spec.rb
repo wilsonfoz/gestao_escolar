4f56ea658b2e725fba0cef207b1a35c3b7000008
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 22:13:37 -0300

    Implementado teste unitário para aula.

--- spec/models/aula_spec.rb
+++ spec/models/aula_spec.rb
@@ -1,5 +1,20 @@
+#encoding: UTF-8
 require 'spec_helper'
 
 describe Aula do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:materia) }
+  it { should belong_to(:professor) }
+  it { should belong_to(:turma) }
+    
+  it "não deve ter matérias duplicadas por turma" do
+  	aula1 = FactoryGirl.create(:aula, materia_id: 1, professor_id: 1,  turma_id: 1 )
+  	aula2 = FactoryGirl.build(:aula, materia_id: 1, professor_id: 2,  turma_id: 1 )
+  	aula2.should_not be_valid
+  end
+
+  it "to_s deve retornar nome do professor e nome da matéria da aula" do
+  	aula = FactoryGirl.create(:aula)
+  	aula.to_s.should == "#{aula.professor} - #{aula.materia}"
+  end
+
 end
