562c9066495a03fbfe115078bb947bb8920a373b
Wilson Jung <wilsonfoz@gmail.com>
2012-05-29 21:44:40 -0300

    Implementado teste unitário para matéria.

--- spec/models/materia_spec.rb
+++ spec/models/materia_spec.rb
@@ -1,5 +1,23 @@
+# encoding: UTF-8
 require 'spec_helper'
 
 describe Materia do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:aulas) }
+  it { should have_many(:professores).through(:aulas) }
+  it { should have_many(:turmas).through(:aulas) }
+
+  it "deve possuir um nome" do
+	Materia.new(:nome => "").should_not be_valid
+  end
+
+  it "deve possuir um nome único" do
+  	Materia.create(:nome => "Teste")
+  	should validate_uniqueness_of(:nome)
+  end
+
+  it "to_s deve retornar o nome" do
+	materia = Materia.create(:nome => "Matemática")
+	materia.to_s.should == "Matemática"
+  end
+
 end
